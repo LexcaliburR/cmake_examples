e1fde988902c4c56ba2bc5b2143f37b89331da55
lexcaliburr <lishiqi0111@gmail.com>
2021-10-05 19:16:06 +0800

    add: example to set build type

---
+++ 01-basic/F-build-type/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// argc: argument count
+// argv: argument vector, is a one-dimensional array of strings
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello Build Type! " << std::endl;
+    return 0;
+}
